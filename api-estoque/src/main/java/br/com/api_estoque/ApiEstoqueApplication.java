b3d065c9f6952416266db642f2ff006f62227e77
Victor Maciel <31293720+victor-maciel@users.noreply.github.com>
2026-07-21 21:33:37 -0300

    Commit Inicial
    adicionado as APIs para os estudos  relacionado a Kafka.

---
+++ api-estoque/src/main/java/br/com/api_estoque/ApiEstoqueApplication.java
@@ -0,0 +1,13 @@
+package br.com.api_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEstoqueApplication.class, args);
+	}
+
+}
---
+++ api-pedidos/src/test/java/br/com/api_pedidos/ApiPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
